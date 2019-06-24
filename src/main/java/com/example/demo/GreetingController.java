fea2e79ef435d2bb862a771fdc07aeb9af25c5d9
Paul Bartlett <paul.bartlett@walmartlabs.com>
2019-06-24 10:22:29 -0700

    Add prop and env

--- src/main/java/com/example/demo/GreetingController.java
+++ src/main/java/com/example/demo/GreetingController.java
@@ -2,9 +2,10 @@ package com.example.demo;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Set;
 import java.util.concurrent.atomic.AtomicLong;
+import java.util.stream.Collectors;
 
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -20,9 +21,31 @@ public class GreetingController {
   private final AtomicLong counter = new AtomicLong();
 
   @RequestMapping("/greeting")
-  public Greeting greeting(@RequestParam(value="name", defaultValue="World") String name) {
+  public Greeting greeting(@RequestParam(value = "name", defaultValue = "World") String name) {
     return new Greeting(counter.incrementAndGet(),
         String.format(template, name));
+  }
+
+  private String getMapDisplay(String title, Set set1) {
+    String header = String.format("<h1>%s</h1>", title);
+    Set<Map.Entry<String, String>> set = set1;
+
+    return header  +
+        set.stream()
+            .map( entry -> "<b>" + entry.getKey() + "</b>: " + entry.getValue())
+            .collect(Collectors.joining("<br>"));
+  }
+
+  // Return Environment Variables
+  @RequestMapping("/env")
+  public String env() {
+    return getMapDisplay("Environment Variables", System.getenv().entrySet());
+  }
+
+  // Return System Properties
+  @RequestMapping("/prop")
+  public String prop() {
+    return getMapDisplay("System Properties", System.getProperties().entrySet());
   }
 
   // Worlds simplest
--- src/main/java/com/example/demo/config/WebAppInitializer.java
+++ src/main/java/com/example/demo/config/WebAppInitializer.java
@@ -13,8 +13,11 @@ public class WebAppInitializer implements WebApplicationInitializer {
         AnnotationConfigWebApplicationContext ctx = new AnnotationConfigWebApplicationContext();
         ctx.register(AppConfig.class);
         ctx.setServletContext(servletContext);
+
         Dynamic dynamic = servletContext.addServlet("dispatcher", new DispatcherServlet(ctx));
         dynamic.addMapping("/");
         dynamic.setLoadOnStartup(1);
-   }
+
+//        metadata-complete="true"
+    }
 }
